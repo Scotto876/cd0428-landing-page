c91e3f168b6b84b52520817dcaa5db39c591e9fd
scotto876 <132853026+Scotto876@users.noreply.github.com>
2023-12-01 18:06:20 -0600

    finished product

--- js/app.js
+++ js/app.js
@@ -36,7 +36,48 @@
  */
 
 // build the nav
+function createNav() {
+  // Get the navbar__list element
+  const navbarList = document.getElementById("navbar__list");
+  // Get all section elements
+  const sections = document.querySelectorAll("section");
+  sections.forEach((section) => {
+    // Retrieve the section's id and data-nav attributes
+    const sectionId = section.id;
+    const sectionDataNav = section.getAttribute("data-nav");
 
+    // Create a list item element
+    const listItem = document.createElement("li");
+
+    // Create an anchor element
+    const anchor = document.createElement("a");
+
+    // Set textContent to the value of the section's data-nav attribute
+    anchor.textContent = sectionDataNav;
+
+    // Set href to the value of the section's id attribute
+    anchor.href = `#${sectionId}`;
+
+    // Add the menu__link class to the anchor element
+    anchor.classList.add("menu__link");
+
+    // Add a click event listener to the anchor element
+    anchor.addEventListener("click", (event) => {
+      // Prevent the default click behavior (navigation)
+      event.preventDefault();
+
+      // Scroll to the corresponding section smoothly
+      section.scrollIntoView({ behavior: "smooth" });
+    });
+
+    // Append the anchor element to the list item element
+    listItem.append(anchor);
+
+    // Append the list item element to the navbar__list element
+    navbarList.appendChild(listItem);
+  });
+}
+createNav();
 // Add class 'active' to section when near top of viewport
 
 // Scroll to anchor ID using scrollTO event
@@ -54,74 +95,43 @@
 // Set sections as active
 
 // Get the navbar__list element
-const navbarList = document.getElementById("navbar__list");
-// Get all section elements
-const sections = document.querySelectorAll("section");
-sections.forEach((section) => {
-  // Retrieve the section's id and data-nav attributes
-  const sectionId = section.id;
-  const sectionDataNav = section.getAttribute("data-nav");
 
-  // Create a list item element
-  const listItem = document.createElement("li");
+function sectionHighlight() {
+  // Get all section elements
+  const sections = document.querySelectorAll("section");
+  // Get all anchor elements in navbar
+  const allAnchors = document.querySelectorAll(".menu__link");
+  // Add window scroll event listener
+  window.addEventListener("scroll", () => {
+    // Iterate over each section
+    sections.forEach((section, index) => {
+      // Retrieve the section's position on the page
+      const sectionBounds = section.getBoundingClientRect();
 
-  // Create an anchor element
-  const anchor = document.createElement("a");
+      // Remove existing active class from  section
+      section.classList.remove("nav-active");
 
-  // Set textContent to the value of the section's data-nav attribute
-  anchor.textContent = sectionDataNav;
+      // Check if the section is visible in the viewport
+      if (
+        sectionBounds.top >= 0 &&
+        sectionBounds.left >= 0 &&
+        sectionBounds.bottom <= window.innerHeight &&
+        sectionBounds.right <= window.innerWidth
+      ) {
+        // Add the active class to the section
+        section.classList.add("nav-active");
 
-  // Set href to the value of the section's id attribute
-  anchor.href = `#${sectionId}`;
-
-  // Add the menu__link class to the anchor element
-  anchor.classList.add("menu__link");
-
-  // Add a click event listener to the anchor element
-  anchor.addEventListener("click", (event) => {
-    // Prevent the default click behavior (navigation)
-    event.preventDefault();
-
-    // Scroll to the corresponding section smoothly
-    section.scrollIntoView({ behavior: "smooth" });
+        // adds active class if true, removes active class if false
+        allAnchors.forEach((anchor, anchorIndex) => {
+          if (index === anchorIndex) {
+            anchor.classList.add("nav-active");
+          } else {
+            anchor.classList.remove("nav-active");
+          }
+        });
+      }
+    });
   });
+}
 
-  // Append the anchor element to the list item element
-  listItem.append(anchor);
-
-  // Append the list item element to the navbar__list element
-  navbarList.appendChild(listItem);
-});
-// Get all anchor elements in navbar
-const allAnchors = document.querySelectorAll(".menu__link");
-// Add window scroll event listener
-window.addEventListener("scroll", () => {
-  // Iterate over each section
-  sections.forEach((section, index) => {
-    // Retrieve the section's position on the page
-    const sectionBounds = section.getBoundingClientRect();
-
-    // Remove existing active class from  section
-    section.classList.remove("nav-active");
-
-    // Check if the section is visible in the viewport
-    if (
-      sectionBounds.top >= 0 &&
-      sectionBounds.left >= 0 &&
-      sectionBounds.bottom <= window.innerHeight &&
-      sectionBounds.right <= window.innerWidth
-    ) {
-      // Add the active class to the section
-      section.classList.add("nav-active");
-
-      // adds active class if true, removes active class if false
-      allAnchors.forEach((anchor, anchorIndex) => {
-        if (index === anchorIndex) {
-          anchor.classList.add("nav-active");
-        } else {
-          anchor.classList.remove("nav-active");
-        }
-      });
-    }
-  });
-});
+sectionHighlight();
